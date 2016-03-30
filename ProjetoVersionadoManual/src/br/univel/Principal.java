41863faf9386e9b69f17bc464724a76229c6359b
Danny Felipe <dannyfmello@gmail.com>
2016-03-30 20:07:49 -0300

    Manutenção do código.

--- ProjetoVersionadoManual/src/br/univel/Principal.java
+++ ProjetoVersionadoManual/src/br/univel/Principal.java
@@ -4,8 +4,6 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
-		//System.out.println("Olá mundo Git.");
-		
 		String world = "World";
 		String hello = "Hello";
 		String msg = hello + world + "!!!";
